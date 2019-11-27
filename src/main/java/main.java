d53087c2a2590ab42479cd9f7788536aedb2b083
ferruzzi <ferruzzi@amazon.com>
2019-11-26 22:32:10 -0800

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,19 @@
+public class main {
+
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        System.out.println(String.format("%s Java %s", str1, str2));
+    }
+
+    void method() {
+    }
+
+    void method2(){
+    }
+
+    void method3(){
+
+    }
+
+}
